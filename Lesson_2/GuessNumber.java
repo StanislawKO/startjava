9062270f80c01718c4064c36875d293cbfcb7bb2
Дом <printks@yandex.ru>
2020-02-25 23:50:37 +0300

    implement HW_2

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,36 +1,32 @@
 import java.util.Scanner;
 
 public class GuessNumber {
-    public void startGame(Player p1, Player p2) {
+    private Player p1;
+    private Player p2;
+
+    public GuessNumber(Player p1, Player p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public void startGame() {
         Scanner scanner = new Scanner(System.in);
 
-        int number = (int) (Math.random() * 100);
+        int number = (int) (Math.random() * 101);
         System.out.println("Загаданное число " + number);
 
         while (true) {
             System.out.print("Первый игрок вводит число: ");
             p1.setNumber(scanner.nextInt());
             int fistGuess = p1.getNumber();
-            if (fistGuess != number) {
-                if (fistGuess > number) {
-                    System.out.println("Введенное вами число больше того, что загадал компьютер");
-                } else {
-                    System.out.println("Введенное вами число меньше того, что загадал компьютер");
-                }
-            } else {
+            if (fistGuess == number) {
                 System.out.println("Угадал первый игрок!");
                 break;
             }
             System.out.print("Второй угрок вводит число: ");
             p2.setNumber(scanner.nextInt());
             int secondGuess = p2.getNumber();
-            if (secondGuess != number) {
-                if (secondGuess > number) {
-                    System.out.println("Введенное вами число больше того, что загадал компьютер");
-                } else {
-                    System.out.println("Введенное вами число меньше того, что загадал компьютер");
-                }
-            } else {
+            if (secondGuess == number) {
                 System.out.println("Угадал второй игрок!");
                 break;
             }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 public class GuessNumberTest {
+    private static Player p1;
+    private static Player p2;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите имя первого игрока: ");
-        Player p1 = new Player(scanner.nextLine());
+        p1 = new Player(scanner.nextLine());
         System.out.print("Введите имя второго игрока: ");
-        Player p2 = new Player(scanner.nextLine());
+        p2 = new Player(scanner.nextLine());
         System.out.println("Играют " + p1.getName() + " и " + p2.getName());
 
         String answer;
         do {
-            GuessNumber game = new GuessNumber();
-            game.startGame(p1, p2);
+            GuessNumber game = new GuessNumber(p1, p2);
+            game.startGame();
 
             do {
                 System.out.print("Хотите продолжить? [да/нет]: ");
