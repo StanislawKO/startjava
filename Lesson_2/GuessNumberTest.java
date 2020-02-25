146e0a3316c561cdd77b2117da27eec1c528c261
Дом <printks@yandex.ru>
2020-02-25 23:59:11 +0300

    implement HW_2

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
 public class GuessNumberTest {
-    private static Player p1;
-    private static Player p2;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите имя первого игрока: ");
-        p1 = new Player(scanner.nextLine());
+        Player p1 = new Player(scanner.nextLine());
         System.out.print("Введите имя второго игрока: ");
-        p2 = new Player(scanner.nextLine());
+        Player p2 = new Player(scanner.nextLine());
         System.out.println("Играют " + p1.getName() + " и " + p2.getName());
 
         String answer;
